29e0cb0c4ed10f42eb9ea1ad4095a29dee93633c
Theyellowflashminato <91362584+Theyellowflashminato@users.noreply.github.com>
2021-11-27 16:27:32 +0530

    Create multi.cpp

---
+++ multi.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class student{
+private:
+int id;
+char name[20];
+public:
+float m1,m2,m3;
+void getstu(){
+cout<<"enter id and name"<<endl;
+cin>>id>>name;
+}
+void putstu(){
+cout<<"id is"<<id<<endl;
+cout<<"name is"<<name<<endl;
+}
+};
+class marks :public student
+{
+public:
+void getmarks()
+{
+cout<<"enter marks of 3 subjects"<<endl;
+cin>>m1>>m2>>m3;
+
+}
+void putmarks()
+{
+cout<<"marks are"<<"1."<<m1<<"2."<<m2<<"3."<<m3;
+}
+};
+class result:public marks{
+
+public:
+void tmarks()
+{
+getstu();
+getmarks();
+putstu();
+putmarks();
+cout<<"total marks is :"<<m1+m2+m3;
+cout<<"average is :"<<(m1+m2+m3)/3.0;
+}
+};
+main()
+{
+result p;
+p.tmarks();
+}
